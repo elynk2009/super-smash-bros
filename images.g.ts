85bcdea5e7f652111b9a10b510a5502660dd1719
elynk2009 <155399279+elynk2009@users.noreply.github.com>
2024-07-08 17:32:14 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,94 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "OG!K`c,stS8teITP^u4{":
+            case "Opponent Link":return img`
+. . . . . . 7 7 7 7 3 . . . . 
+. . . e e e e 7 7 7 7 7 . . . 
+. . e e e e e e 7 7 d 7 7 7 . 
+. . . e e e e e e d d 7 7 7 7 
+e . . d 7 d d e d d d 7 7 . 7 
+e d d d e d d e d d e e 7 . . 
+e . . d d d d d d e e e . . . 
+e . . d d d d 7 7 7 7 . . . . 
+e d e e e 7 7 7 7 7 7 7 e . . 
+e d e e 7 7 7 d d d 7 e e e . 
+e . e e 7 7 7 d d d e e e e . 
+e . . . e 7 7 7 d d e e e e . 
+e . . . e e e e 7 7 e e 7 . . 
+. . . . 7 7 7 7 7 7 7 7 7 7 . 
+. . . . . . . e e e e . . . . 
+. . . . . . e e e e e . . . . 
+`;
+            case "d4j1pveRC*nb=@U+D{HZ":
+            case "Opponent Mario":return img`
+. . . . 2 2 2 2 2 . . . 
+. 2 2 2 2 2 2 2 2 2 . . 
+. . . . f d d e e e . . 
+. d d d f d d d e d e . 
+d d d f d d d e e d e . 
+. f f f f d d d d e . . 
+. . . d d d d d d . . . 
+. . 2 2 8 2 2 8 2 2 . . 
+. 2 2 2 8 2 2 8 2 2 2 . 
+2 2 2 2 8 8 8 8 2 2 2 2 
+d d 2 8 5 8 8 5 8 2 d d 
+d d d 8 8 8 8 8 8 d d d 
+d d 8 8 8 8 8 8 8 8 d d 
+. . 8 8 8 . . 8 8 8 . . 
+. e e e . . . . e e e . 
+e e e e . . . . e e e e 
+`;
+            case "2baV9I/RH^N8BtD?Lj)a":
+            case "Opponent Pikachu":return img`
+. . . . . . . f . . . . . 
+f f f . . . f 5 f . f f f 
+f f 5 f . . f 5 5 f 5 f f 
+. f 5 5 f f f f f 5 5 f . 
+. . f 5 5 5 5 5 5 5 f . . 
+. . f 5 f 5 5 5 f 5 f . . 
+. . f 5 5 5 f 5 5 5 f . . 
+. f 5 2 5 f f f 5 2 5 f . 
+. f 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 f 5 5 5 f 5 5 f . 
+. f 5 f f 5 5 5 f f 5 f . 
+. f 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 f f f 5 5 f . . 
+. . . f f . . . f f . . . 
+`;
+            case "M3o7viS2w2Wpe4hqBtP(":
+            case "Opponent Captain Falcon":return img`
+. . . . . 2 2 2 . . . . . 
+. . . . 2 1 2 1 2 . . . . 
+. . . 2 f 1 6 1 f 2 . . . 
+. . . 2 f f 1 f f 2 . . . 
+. . . 2 f 7 f 7 f 2 . . . 
+. . . 2 f f 2 f f 2 . . . 
+. . . 2 f 2 2 2 f 2 . . . 
+. . . . 2 d d d 2 . . . . 
+. . . . . 2 2 2 . . . . . 
+. . . . . . d . . . . . . 
+. . . . . d d d . . . . . 
+. . . d d d d d d d . . . 
+. . d . f f f f f . d . . 
+2 2 . . f f f f f . . 2 2 
+2 2 . . . f f f . . . 2 2 
+. . . . . f . f . . . . . 
+. . . . 2 2 . 2 2 . . . . 
+. . . 2 2 2 . 2 2 2 . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
