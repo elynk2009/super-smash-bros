6987d4c4fe7682538e3bda365bd2d0bba856da51
elynk2009 <155399279+elynk2009@users.noreply.github.com>
2025-10-26 22:08:15 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "d4j1pveRC*nb=@U+D{HZ":
-            case "Opponent Mario":return img`
-. . . . 2 2 2 2 2 . . . 
-. 2 2 2 2 2 2 2 2 2 . . 
-. . . . f d d e e e . . 
-. d d d f d d d e d e . 
-d d d f d d d e e d e . 
-. f f f f d d d d e . . 
-. . . d d d d d d . . . 
-. . 2 2 8 2 2 8 2 2 . . 
-. 2 2 2 8 2 2 8 2 2 2 . 
-2 2 2 2 8 8 8 8 2 2 2 2 
-d d 2 8 5 8 8 5 8 2 d d 
-d d d 8 8 8 8 8 8 d d d 
-d d 8 8 8 8 8 8 8 8 d d 
-. . 8 8 8 . . 8 8 8 . . 
-. e e e . . . . e e e . 
-e e e e . . . . e e e e 
-`;
             case "2baV9I/RH^N8BtD?Lj)a":
             case "Opponent Pikachu":return img`
 . . . . . . . f . . . . . 
@@ -62,22 +43,41 @@ f f 5 f . . f 5 5 f 5 f f
 `;
             case "OG!K`c,stS8teITP^u4{":
             case "Opponent Link":return img`
-. . . . . . 7 7 7 7 3 . . . . 
-. . . e e e e 7 7 7 7 7 . . . 
-. . e e e e e e 7 7 d 7 7 7 . 
-. . . e e e e e e d d 7 7 7 7 
-e . . d 7 d d e d d d 7 7 . 7 
-e d d d e d d e d d e e 7 . . 
-e . . d d d d d d e e e . . . 
-e . . d d d d 7 7 7 7 . . . 3 
-e d e e e 7 7 7 7 7 7 7 e . 3 
-e d e e 7 7 7 d d d 7 e e e . 
-e . e e 7 7 7 d d d e e e e . 
-e . . . e 7 7 7 d d e e e e . 
-e . . . e e e e 7 7 e e 7 . . 
-. . . . 7 7 7 7 7 7 7 7 7 7 . 
-. . . . . . . e e e e . . . . 
-. . . . . . e e e e e . . . . 
+. . . . . 7 7 7 7 . . . . . . 
+. . . 7 7 7 7 7 e e e e . . . 
+. 7 7 7 d 7 7 e e e e e e . . 
+7 7 7 7 d d e e e e e e . . . 
+7 . 7 7 d d d e d d 7 d . . e 
+. . 7 e e d d e d d e d d d e 
+. . . e e e d d d d d d . . e 
+. . . . 7 7 7 7 d d d d . . e 
+. . e 7 7 7 7 7 7 7 e e e d e 
+. e e e 7 d d d 7 7 7 e e d e 
+. e e e e d d d 7 7 7 e e . e 
+. e e e e d d 7 7 7 e . . . e 
+. . 7 e e 7 7 e e e e . . . e 
+. 7 7 7 7 7 7 7 7 7 7 . . . . 
+. . . . e e e e . . . . . . . 
+. . . . e e e e e . . . . . . 
+`;
+            case "d4j1pveRC*nb=@U+D{HZ":
+            case "Opponent Mario":return img`
+. . . 2 2 2 2 2 . . . . 
+. . 2 2 2 2 2 2 2 2 2 . 
+. . e e e d d f . . . . 
+. e d e d d d f d d d . 
+. e d e e d d d f d d d 
+. . e d d d d f f f f . 
+. . . d d d d d d . . . 
+. . 2 2 8 2 2 8 2 2 . . 
+. 2 2 2 8 2 2 8 2 2 2 . 
+2 2 2 2 8 8 8 8 2 2 2 2 
+d d 2 8 5 8 8 5 8 2 d d 
+d d d 8 8 8 8 8 8 d d d 
+d d 8 8 8 8 8 8 8 8 d d 
+. . 8 8 8 . . 8 8 8 . . 
+. e e e . . . . e e e . 
+e e e e . . . . e e e e 
 `;
         }
         return null;
